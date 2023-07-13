989b9c8a214c6e807b7e5a52a75c5dd868307c52
paposeco <kogepan@disroot.org>
2023-07-13 11:22:35 +0100

    user info and profile pic are requested from the api

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -8,11 +8,14 @@ interface FuncProps {
 const Homepage: React.FC<FuncProps> = (props) => {
     const [token, setToken] = useState("");
     const [tokenFetched, setTokenFetched] = useState(false);
+    const [facebookID, setFacebookID] = useState("");
+    const [userName, setUserName] = useState("");
+    const [profilePic, setProfilePic] = useState("");
 
     useEffect(() => {
-        const fetchInfo = async function(bearertoken: string) {
+        const fetchInfo = async function(bearertoken: string, userFacebookID: string) {
             try {
-                const response = await fetch("http://localhost:3000/jwttest", {
+                const response = await fetch("http://localhost:3000/" + userFacebookID, {
                     method: "GET",
                     headers: {
                         "Content-Type": "application/json",
@@ -20,7 +23,9 @@ const Homepage: React.FC<FuncProps> = (props) => {
                     }
                 });
                 const responseData = await response.json();
-                console.log(responseData);
+
+                setUserName(responseData.userInfo.display_name);
+                // api request for file
             } catch (err) {
                 console.log(err);
             }
@@ -28,13 +33,40 @@ const Homepage: React.FC<FuncProps> = (props) => {
         if (document.cookie !== "" && !tokenFetched) {
             setTokenFetched(true);
             const fullCookie = document.cookie;
-            const cleanCookie = fullCookie.slice(6, fullCookie.length);
-            setToken(cleanCookie);
-            props.updateToken(cleanCookie);
-            localStorage.setItem("token", cleanCookie);
-            fetchInfo(cleanCookie);
+            const semiColon = document.cookie.indexOf(";");
+            const cleanTokenCookie = fullCookie.slice(6, semiColon);
+            const cleanFacebookIdCookie = fullCookie.slice(semiColon + 13, fullCookie.length);
+            setToken(cleanTokenCookie);
+            setFacebookID(cleanFacebookIdCookie);
+            props.updateToken(cleanTokenCookie);
+            localStorage.setItem("token", cleanTokenCookie);
+            fetchInfo(cleanTokenCookie, cleanFacebookIdCookie);
         }
     }, []);
+
+    useEffect(() => {
+        const fetchPic = async function(bearertoken: string, userFacebookID: string) {
+            try {
+                const response = await fetch(
+                    "http://localhost:3000/" + userFacebookID + "/profilepic",
+                    {
+                        method: "GET",
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${bearertoken}`
+                        }
+                    }
+                );
+                const blob = await response.blob();
+                setProfilePic(URL.createObjectURL(blob));
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        if (token !== "" && facebookID !== "") {
+            fetchPic(token, facebookID);
+        }
+    }, [facebookID, token]);
 
     // once token is fetched, query api for content
     if (token === "") {
@@ -48,7 +80,8 @@ const Homepage: React.FC<FuncProps> = (props) => {
         return (
             <div>
                 <h1>Homepage</h1>
-                <p>Logged in</p>
+                <p>Hello {userName}</p>
+                <img src={profilePic} alt="profile pic" />
             </div>
         );
     }
