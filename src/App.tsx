c8639ef053369509e4e9e29c7a3f5afa234b894c
paposeco <13892562+paposeco@users.noreply.github.com>
2023-08-29 16:09:03 +0200

    removed react store

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "components/Login";
 import Homepage from "components/Homepage";
 import What from "components/What";
 import Loggedin from "components/Loggedin";
 import NewPost from "components/NewPost";
+import type { UserProfile } from "common/types";
 
 const App: React.FC = () => {
     const [token, setToken] = useState("");
+    const [facebookID, setFacebookID] = useState("");
 
+    // user info TYPE
+    // const [userInfo, setUserInfo]
+    const apiURL = "http://localhost:3000/";
     const authBearerToken = function(childtoken: string): void {
         // not necessary. to be removed later
         if (token === "") {
             setToken(childtoken);
         }
     };
-    const apiURL = "http://localhost:3000/";
+
+    // profile and timeline info should be fetched here and made available to components
+
+    // at this point, facebookid should be on a cookie?
+    useEffect(() => {
+        if (token !== "") {
+        }
+        const fetchUserInfo = async function() {
+            try {
+                const response = await fetch(apiURL + facebookID + "/homepage", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+                const responseData = await response.json();
+                // set user info type
+            } catch (err) {
+                console.log(err);
+            }
+        };
+    }, [token]);
+
     return (
         <div id="content">
             <Routes>
