4d1321102b7c34405f7a6cf71ce695d078e03f52
paposeco <kogepan@disroot.org>
2023-11-21 11:51:04 +0000

    back to ghpages

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, useLocation } from "react-router-dom";
+import {BrowserRouter, useLocation, HashRouter} from "react-router-dom";
 import App from "./App";
 import "./styles/stylesheet.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 export default function ScrollToTop() {
-    const { pathname } = useLocation();
+    const {pathname} = useLocation();
 
     useEffect(() => {
         window.scrollTo(0, 0);
@@ -19,9 +19,13 @@ export default function ScrollToTop() {
 //basename to githubpages
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename={"https://odinbook-production-0a2a.up.railway.app/"}>
+        <HashRouter basename="/odinbook">
             <ScrollToTop />
             <App />
-        </BrowserRouter>
+        </HashRouter>
+        {/* <BrowserRouter basename={"https://odinbook-production-0a2a.up.railway.app/"}>
+            <ScrollToTop />
+            <App />
+        </BrowserRouter> */}
     </React.StrictMode>
 );
